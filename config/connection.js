58e16922951ec40a47f3aae3207511e292b18d62
Origin 42 <daveplum1991@gmail.com>
2022-05-25 13:20:39 +0800

    updating ClearDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,20 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB,
+  process.env.USER,
+  process.env.PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.HOST,
     dialect: 'mysql',
     port: 3306,
   }
 );
 
 module.exports = sequelize;
+module.exports = {
+  HOST: "us-cdbr-iron-east-02.cleardb.net",
+  USER: "b7e2437887xxxa",
+  PASSWORD: "0200xxx6",
+  DB: "heroku_7643ec736354xxx"
+};
