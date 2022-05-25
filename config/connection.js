72d4d363d8d7153e70a24f790f81126e4a6ccb66
Origin 42 <daveplum1991@gmail.com>
2022-05-25 14:55:58 +0800

    adding minor server adjustments

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,14 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.USER,
-  process.env.PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.HOST,
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306,
   }
 );
 
 module.exports = sequelize;
-module.exports = {
-  HOST: "us-cdbr-iron-east-02.cleardb.net",
-  USER: "b7e2437887xxxa",
-  PASSWORD: "0200xxx6",
-  DB: "heroku_7643ec736354xxx"
-};
